30b262edb2ff9250a472c0d63292191ef786e7cb
Jon Moliner <jonmolinerzama@gmail.com>
2021-12-21 20:56:06 +0100

    probando

--- seeds/users.seed.js
+++ seeds/users.seed.js
@@ -1,1 +1,50 @@
-// branch feature
+const mongoose = require('mongoose');
+const User = require('../models/user.model');
+
+const MONGO_URI = process.env.MONGODB_URI || 'mongodb://localhost/course-db';
+
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  })
+  .then(() => {
+    console.log('Connected to Mongodb');
+    return User.deleteMany();
+  })
+  .then(() => {
+    return User.insertMany(user)
+  })
+  .then((createdUsers)=> {
+    console.log(`Created ${createdUsers.length} users`);
+  })
+  .then(() => {
+
+    mongoose.connection.close();
+     })
+.catch((error) =>
+console.log(`An error occurred while creating users from the DB: ${error}`)
+);
+
+//optimizar
+
+const user = [
+    {
+    username: JuanJose,
+    email: {
+        type: juanjose@gmail.com,
+        required: true,
+        unique: true,
+    },
+    nif: {
+        type: String,
+        unique: true
+    },
+    dateBirthday: Date,
+    hashedPassword: {
+        type: String,
+        required: [true, 'password is required'],
+    },
+    }
+]
