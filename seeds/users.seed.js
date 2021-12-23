02af79bda85f0e01cb099cba70407b7eaf3f0e82
Jon Moliner <jonmolinerzama@gmail.com>
2021-12-23 20:13:57 +0100

    nuevos fundamentos

--- seeds/users.seed.js
+++ seeds/users.seed.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const User = require('../models/user.model');
 
-const MONGO_URI = process.env.MONGODB_URI || 'mongodb://localhost/course-db';
+const MONGO_URI = process.env.MONGODB_URI || 'mongodb://localhost/27017';
 
 mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
