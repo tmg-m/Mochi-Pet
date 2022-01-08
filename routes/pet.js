3b7c9042a31580b719c15a215bb3651b2d6688f1
Mahendra <bus.mahendrat@gmail.com>
2022-01-08 13:55:04 +0100

    minor adjustments pet

--- routes/pet.js
+++ routes/pet.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const session = require('express-session');
 const { isLoggedIn } = require('../middlewares');
+const Pet = require('../models/pet');
 
 function petRoutes() {
   const router = express.Router();
@@ -11,10 +13,17 @@ function petRoutes() {
   router.post('/pet-create', async (req, res, next) => {
     const { petName, petType, petGender, petColor } = req.body;
 
+    console.log('creating pet');
     try {
 
-        
-
+      await Pet.create({
+        petName,
+        petType,
+        petGender,
+        petColor,
+      });
+      
+      res.render('pet-create', { errorMessage: 'Pet created !' });
     } catch (err) {
       next(err);
     }
