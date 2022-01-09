61e582d0305aa882c2a04a788cf7a6822f8c2c82
Mahendra <bus.mahendrat@gmail.com>
2022-01-09 19:07:59 +0100

    added address,city,age, html adjustments to forms, working scema with db

--- models/pet.js
+++ models/pet.js
@@ -4,17 +4,34 @@ const petSchema = new mongoose.Schema({
   petOwner: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Post',
+    unique: true,
   },
-  petName: {
+  petCategory: {
     type: String,
-  },
-  petType: {
-    type: String,
+    enum: ['Dog', 'Cat', 'Hamster', 'Fish', 'Birds', 'Other'],
+    required: true,
   },
   petGender: {
     type: String,
+    enum: ['Male', 'Female'],
+    required: true,
+  },
+  petName: {
+    type: String,
+    required: true,
+  },
+  petAge: {
+    type: String,
+    required: true,
   },
   petColor: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+  },
+  city: {
     type: String,
   },
 });
