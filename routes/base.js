bc9f3d8cc4a2ea53b9a735a2acb5af87a880fd94
Mahendra <bus.mahendrat@gmail.com>
2022-01-14 14:57:21 +0100

    user crud, done

--- routes/base.js
+++ routes/base.js
@@ -4,17 +4,16 @@ const Pet = require('../models/pet');
 
 function baseRoutes() {
   const router = express.Router();
-
   router.get('/', async (req, res, next) => {
 
     const isLogged = req.session.currentUser;
     try {
+      userId = req.session.currentUser._id;
+      const user = userId;
       //if user is logged in ,line 10-12
       if (isLogged) {
-        console.log('find');
         const pets = await Pet.find({});
-        console.log('pet found');
-        return res.render('home.hbs', { pets });
+        return res.render('home.hbs', { pets, user });
       }
       res.render('index.hbs', { name: 'myApp' });
     } catch (e) {
--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,49 @@
 const express = require('express');
 const { isLoggedIn } = require('../middlewares');
-const Pet = require('../models/pet');
+const User = require('../models/user');
 
 function userRoutes() {
   const router = express.Router();
 
-  router.get('/', (req, res, next) => {
-    return res.render('user/profile.hbs');
+  router.get('/:id', async (req, res, next) => {
+    const { id } = req.params;
+    try {
+      const user = await User.findById(id);
+      res.render('user/profile.hbs', { user });
+    } catch (e) {
+      next(e);
+    }
   });
 
   router.get('/mylist', (req, res, next) => {
     return res.render('user/mylist.hbs');
+  });
+
+  router.get('/:id/userEdit', async (req, res, next) => {
+    const { id } = req.params;
+    try {
+      const user = await User.findById(id);
+      res.render('user/userEdit.hbs', { user });
+    } catch (e) {
+      next(e);
+    }
+  });
+
+  router.post('/:id/userEdit', async (req, res, next) => {
+    const { id } = req.params;
+    const { username, name, email } = req.body;
+
+    try {
+      console.log('editing');
+      await User.findByIdAndUpdate(id, {
+        username,
+        name,
+        email,
+      });
+      res.redirect('/');
+    } catch (e) {
+      next(e);
+    }
   });
 
   return router;
