bbd43067f9234c11ba86ab4234d2b05f68352881
zapatran <zapatran@users.noreply.github.com>
2022-01-15 13:14:04 +0100

    add search

--- routes/base.js
+++ routes/base.js
@@ -6,15 +6,25 @@ function baseRoutes() {
   const router = express.Router();
 
   router.get('/', async (req, res, next) => {
+    const { category, gender } = req.query;
+
+    const query = {};
+
+    if (category) {
+      query.petCategory = category;
+    }
+    if (gender) {
+      query.petGender = gender;
+    }
+
     try {
-      //if user is logged in ,line 10-12
-      if (req.session.currentUser) {
-        console.log('find');
-        const pets = await Pet.find({});
-        console.log('pet found');
-        return res.render('home.hbs', { pets });
-      }
-      res.render('index.hbs', { name: 'myApp' });
+      // if user is logged in ,line 10-12
+      const isLogged = req.session.currentUser;
+
+      const pets = await Pet.find(query);
+
+      return res.render('home.hbs', { pets, isLogged, query });
+      // res.render('index.hbs', { name: 'myApp' }); // TODO solo puede haber un render
     } catch (e) {
       next(e);
     }
